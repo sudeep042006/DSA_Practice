b93680a44051af7b6bce7f2c067e5607be0c4492
sudeep042006 <sudeepkuralkar02@gmail.com>
2026-05-07 23:34:39 +0530

    LC217

---
+++ leetcode/217.contains-duplicate.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=217 lang=java
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+         HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            if(set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
+}
+// @lc code=end
+
+// learn the concept of hashset list<integer and many more about it.. >
